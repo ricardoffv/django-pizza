a583d531eb95fff57d623223fb6fc20a222d112d
Ricardo do Vale <ricardo.vale@usp.br>
2015-08-18 10:48:08 -0300

    updated file]

---
+++ loops.py
@@ -0,0 +1,13 @@
+pedidos = [
+    {
+        'nome': 'mario'
+        'sabor': 'portuguesa'
+    }
+    {
+        'nome': 'Marco'
+        'sabor': 'presunto'
+    }
+]
+
+for pedido in pedidos:
+    print(pedido['nome'], pedido['sabor'])
