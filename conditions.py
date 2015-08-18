23e7bba93f413c23bd9447fb7e3fa6fb4694d131
Ricardo do Vale <ricardo.vale@usp.br>
2015-08-18 10:50:57 -0300

    new file again

--- conditions.py
+++ conditions.py
@@ -1,1 +1,19 @@
 print ('Hello World')
+
+sauce = 'tomato'
+
+description = """
+What are you doing with these pizza,
+random text to show that you can stor big texts in variables
+"""
+
+order_number = 42
+
+order_number = 'not a number anymore'
+
+if sauce.startswith('toma'):
+    print("It's probably a tomato...")
+else:
+    print("not tomato")
+
+print description
