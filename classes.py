348b0feca3f07d74fc2d6c23b87294c95f0746b6
Ricardo do Vale <ricardo.vale@usp.br>
2015-08-18 11:50:45 -0300

    classes

---
+++ classes.py
@@ -0,0 +1,8 @@
+
+class Pedidos(object):
+    def __init__(self, nome, endereco, obs): # criacao da instancia, incializacao dos atributos
+        self.nome = nome
+        self.endereco = endereco
+        self.obs = obs
+    
+    
