2c66723efb15b165f2ea0181b843383df77d2fd7
Ricardo do Vale <ricardo.vale@usp.br>
2015-08-18 15:40:36 -0300

    pizza mais

--- pizzaria/pizza/admin.py
+++ pizzaria/pizza/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from .models import Pizza
 
 
-admin.site.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+    search_fields = ['flavor', 'ingredients']
+    list_display = ['flavor', 'ingredients', 'price']
+
+
+admin.site.register(Pizza, PizzaAdmin)
